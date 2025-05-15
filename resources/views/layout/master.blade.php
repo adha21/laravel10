b6487e9381e7ea2b6204ddd2aa40133a69be2f2e
adha21 <adhamastalulala@gmail.com>
2025-05-15 19:09:24 +0700

    dosen controller

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -39,7 +39,7 @@
     @yield('konten')
 
     <footer class="fixed-bottom bg-dark text-white py-2 mt-4 text-center">
-        Copyright &copy; Maulian Saputra - 2025
+        Copyright &copy; Adha Mastalul A'la - 2025
     </footer>
 
 
